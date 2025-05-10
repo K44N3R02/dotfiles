fa9f6fcdc6e460c81f247ad3eb046ca5c2af90f6
k44n <kaanemireroz@gmail.com>
2025-05-10 19:33:44 +0300

    add new fugitive keymaps

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -26,6 +26,8 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+      vim.keymap.set('n', '<leader>ga', '<CMD>G add %<CR>')
+      vim.keymap.set('n', '<leader>gc', '<CMD>G commit<CR>')
     end,
   },
 
