54b24e72c1b705abac3980aaa65f06508ffe046e
k44n <kaanemireroz@gmail.com>
2026-02-02 16:10:43 +0300

    change sketchybar theme to rosepine

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -16,25 +16,55 @@
 # export BG2=0xff414550
 
 ### Catppuccin
-export BLACK=0xff181926
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
-export TRANSPARENT=0x00000000
-export BG0=0xff1e1e2e
-export BG1=0x603c3e4f
-export BG2=0x60494d64
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
 
-export BATTERY_1=0xffa6da95
-export BATTERY_2=0xffeed49f
-export BATTERY_3=0xfff5a97f
-export BATTERY_4=0xffee99a0
-export BATTERY_5=0xffed8796
+### Rose Pine Moon
+export BASE=0xff232136
+export SURFACE=0xff2a273f
+export OVERLAY=0xff393552
+export MUTED=0xff6e6a86
+export TEXT=0xffe0def4
+export LOVE=0xffeb6f92
+export GOLD=0xfff6c177
+export ROSE=0xffea9a97
+export PINE=0xff3e8fb0
+export FOAM=0xff9ccfd8
+export IRIS=0xffc4a7e7
+export HIGHLIGHT_LOW=0xff2a283e
+export HIGHLIGHT_MED=0xff44415a
+export HIGHLIGHT_HIGH=0xff56526e
+
+export BLACK=$BASE
+export WHITE=$TEXT
+export RED=$LOVE
+export GREEN=$PINE
+export BLUE=$FOAM
+export YELLOW=$GOLD
+export ORANGE=$ROSE
+export MAGENTA=$IRIS
+export GREY=$MUTED
+export TRANSPARENT=0x00000000
+export BG0=$BASE
+export BG1=$SURFACE
+export BG2=$OVERLAY
+
+export BATTERY_1=$PINE
+export BATTERY_2=$FOAM
+export BATTERY_3=$GOLD
+export BATTERY_4=$ROSE
+export BATTERY_5=$LOVE
 
 # General bar colors
 export BAR_COLOR=$BG0
